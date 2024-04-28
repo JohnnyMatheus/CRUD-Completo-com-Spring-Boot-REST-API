e861fcbdb5c85b1a4c1ef3949b0e234a522835cc
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2024-04-28 05:25:26 -0300

    update
     Criando END-POINT de listar todos usuários

--- curso-dev-treinamento/src/main/java/br/com/alunoti/curso_dev_treinamento/controllers/GreetingsController.java
+++ curso-dev-treinamento/src/main/java/br/com/alunoti/curso_dev_treinamento/controllers/GreetingsController.java
@@ -1,10 +1,15 @@
 package br.com.alunoti.curso_dev_treinamento.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +22,10 @@ import br.com.alunoti.curso_dev_treinamento.repository.UsuarioRepository;
  */
 @RestController
 public class GreetingsController {
-	@Autowired /* IC/CD OU CDI - injeção de dependencia */
+	
+	@Autowired /* IC/CD OU CDI - injeção de dependencia*/
 	private UsuarioRepository usuarioRepository;
+	
 
 	/**
 	 *
@@ -34,11 +41,23 @@ public class GreetingsController {
 	@RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public String retornaOlaMundo(@PathVariable String nome) {
+		
 		Usuario usuario = new Usuario();
 		usuario.setNome(nome);
-		usuarioRepository.save(usuario);
-
+		
+		usuarioRepository.save(usuario);/*grava no banco de dados*/
 		return "Ola mundo " + nome;
 	}
+	
+	@GetMapping(value = "listatodos")
+	@ResponseBody /*Retorna os dados para o corpo da resposta -> retorna JSON*/
+	public ResponseEntity<List<Usuario>> listaUsuario(){
+		
+		List<Usuario> usuario = usuarioRepository.findAll();/*EXECUTA A CONSULTA NO BANCO DE DADOS*/
+		
+		return new ResponseEntity<List<Usuario>>(usuario, HttpStatus.OK);/*Retorna a lista em JSON*/
+	}
+	
+	
 
 }
--- curso-dev-treinamento/src/main/java/br/com/alunoti/curso_dev_treinamento/repository/UsuarioRepository.java
+++ curso-dev-treinamento/src/main/java/br/com/alunoti/curso_dev_treinamento/repository/UsuarioRepository.java
@@ -1,5 +1,11 @@
 package br.com.alunoti.curso_dev_treinamento.repository;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.alunoti.curso_dev_treinamento.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
 
 }
